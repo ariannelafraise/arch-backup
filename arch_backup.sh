ea30bfd691379f0dd7b4ed33cefa2178345bc9dd
Arianne <github@shield.paintilya.dev>
2025-03-30 18:45:48 -0400

    2025-03-30

--- arch_backup.sh
+++ arch_backup.sh
@@ -8,11 +8,11 @@ cp -r ~/.config/eww backup/eww
 
 echo "Backup completed!"
 echo "Backup files are stored in the 'backup' directory."
-
+echo ""
 echo "Pushing to Github..."
-
+echo ""
 git add .
 git commit -m "$(date +%Y-%m-%d)"
 git push -u origin main
-
+echo ""
 echo "Push completed!"
