f69b6ee2710f3a2a4586fc34b219a2fe9d51b95d
Arianne <github@shield.paintilya.dev>
2025-03-30 18:47:18 -0400

    2025-03-30

--- arch_backup.sh
+++ arch_backup.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# Pastel colors
+GREEN="\e[38;5;150m"  # Light pastel green
+BLUE="\e[38;5;147m"   # Light pastel blue
+PINK="\e[38;5;218m"   # Soft pink
+YELLOW="\e[38;5;222m" # Light pastel yellow
+RESET="\e[0m"         # Reset color
+
+echo -e "${BLUE}Starting backup...${RESET}"
+
 cp ~/.bashrc backup/.bashrc
 cp -r ~/.config/waybar backup/waybar
 cp -r ~/.config/hypr backup/hypr
 cp -r ~/.config/kitty backup/kitty
 cp -r ~/.config/eww backup/eww
 
-echo "Backup completed!"
-echo "Backup files are stored in the 'backup' directory."
-echo ""
-echo "Pushing to Github..."
-echo ""
+echo -e "${GREEN}Backup completed!${RESET}"
+echo -e "${YELLOW}Backup files are stored in the 'backup' directory.${RESET}\n"
+
+echo -e "${PINK}Pushing to GitHub...${RESET}\n"
+
 git add .
 git commit -m "$(date +%Y-%m-%d)"
 git push -u origin main
-echo ""
-echo "Push completed!"
+
+echo -e "\n${GREEN}Push completed!${RESET}"
