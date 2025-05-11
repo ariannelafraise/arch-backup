2066f8842ba20367a3f67b7a6ae4782832545a0c
ariannelafraise <github@shield.paintilya.dev>
2025-05-11 18:11:19 -0400

    2025-05-11 18:11:19

--- arch_backup.sh
+++ arch_backup.sh
@@ -19,16 +19,17 @@ echo -e "${BLUE}${BACKUP_ICON} Starting backup...${RESET}"
 
 mkdir -p $script_dir/backup/bash
 cp ~/.bashrc $script_dir/backup/bash/.bashrc
-cp -r ~/.config/waybar $script_dir/backup/.config/
-cp -r ~/.config/hypr $script_dir/backup/.config/
-cp -r ~/.config/kitty $script_dir/backup/.config/
-cp -r ~/.config/eww $script_dir/backup/.config/
-cp -r ~/.config/wofi $script_dir/backup/.config/
-cp -r ~/.config/dunst $script_dir/backup/.config/
-cp -r ~/.config/oh-my-posh $script_dir/backup/.config/
-cp -r ~/.config/fastfetch $script_dir/backup/.config/
-cp -r ~/.config/spicetify $script_dir/backup/.config/
-cp -r ~/.config/swaync $script_dir/backup/.config/
+cp -r ~/.config/waybar $script_dir/backup/.config
+cp -r ~/.config/hypr $script_dir/backup/.config
+cp -r ~/.config/kitty $script_dir/backup/.config
+cp -r ~/.config/eww $script_dir/backup/.config
+cp -r ~/.config/wofi $script_dir/backup/.config
+cp -r ~/.config/dunst $script_dir/backup/.config
+cp -r ~/.config/oh-my-posh $script_dir/backup/.config
+cp -r ~/.config/fastfetch $script_dir/backup/.config
+cp -r ~/.config/spicetify $script_dir/backup/.config
+cp -r ~/.config/swaync $script_dir/backup/.config
+cp -r ~/.config/btop $script_dir/backup/.config
 
 echo -e "${GREEN}${DONE_ICON} Backup completed!${RESET}"
 echo -e "${YELLOW}Backup files are stored in the '${script_dir}/backup' directory.${RESET}\n"
